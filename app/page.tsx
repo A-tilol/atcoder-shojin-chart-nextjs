7e8897a8f382d32ba3369f2b4ede4f71583611e3
atilol <ggrkssk@gmail.com>
2024-07-13 19:57:01 +0900

    -

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,181 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import dynamic from "next/dynamic";
+import React, { useState } from "react";
+const Plot = dynamic(() => import("react-plotly.js"), { ssr: false });
+// import {PlotParams} from 'react-plotly.js'
+import { Config, Layout, PlotData } from "plotly.js";
+
+const TitleHeader = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <h1 className="font-serif mb-6 text-3xl font-extrabold md:text-2xl lg:text-3xl">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+          Atcoder
+        </span>{" "}
+        <span className="text-gray-700">精進チャート</span>
+      </h1>
+    </>
+  );
+};
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+interface InputFieldsProps {
+  userID: string;
+  rivalIDs: string;
+  period: number;
+  onInputChange: (id: string, value: string) => void;
+}
+
+const InputFields: React.FC<InputFieldsProps> = ({
+  userID,
+  rivalIDs,
+  period,
+  onInputChange,
+}) => {
+  const handleChange = (e: any) => {
+    console.log(e);
+    onInputChange(e.target.id, e.target.value);
+  };
+
+  return (
+    <>
+      <div className="mb-6">
+        <label
+          htmlFor="user-id-input"
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          UserID
+        </label>
+        <input
+          type="text"
+          id="user-id-input"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
+          value={userID}
+          placeholder="chokudai"
+          onChange={handleChange}
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="mb-6">
+        <label
+          htmlFor="rival-ids-input"
+          className="block mb-2 text-sm font-medium text-gray-900"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          RivalIDs
+        </label>
+        <input
+          type="text"
+          id="rival-ids-input"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
+          value={rivalIDs}
+          placeholder="snuke, rng"
+          onChange={handleChange}
+        />
       </div>
-    </main>
+
+      <div className="mb-6">
+        <label
+          htmlFor="period-input"
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          Period (days)
+        </label>
+        <input
+          type="number"
+          id="period-input"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
+          value={period}
+          min={10}
+          max={10000}
+          step={10}
+          placeholder="90"
+          onChange={handleChange}
+        />
+      </div>
+    </>
+  );
+};
+
+const Content = () => {
+  let [userID, setUserID] = useState("");
+  let [rivalIDs, setRivalIDs] = useState("");
+  let [period, setPeriod] = useState(90);
+
+  const data: Partial<PlotData>[] = [
+    {
+      type: "scatter",
+      mode: "lines",
+      x: [0, 1, 2, 3, 4],
+      y: [10, 4, 2, 6, 7],
+    },
+    {
+      type: "scatter",
+      mode: "lines",
+      x: [0, 1, 2, 3, 4],
+      y: [5, 7, 8, 10, 5],
+    },
+  ];
+  const layout: Partial<Layout> = {
+    title: "Shojin Chart",
+    legend: {
+      title: {
+        text: "Coders",
+      },
+    },
+    margin: {
+      t: 30,
+      b: 0,
+      l: 15,
+      r: 15,
+    },
+  };
+  const config: Partial<Config> = {
+    displayModeBar: false,
+  };
+
+  const handleInputChange = (id: string, value: string) => {
+    console.log(id, value);
+    if (id == "user-id-input") {
+      setUserID(value);
+    } else if (id == "rival-ids-input") {
+      setRivalIDs(value);
+    } else if (id == "period-input") {
+      setPeriod(Number(value));
+    }
+  };
+
+  // if press draw button, draw chart
+
+  return (
+    <>
+      <div className="m-5">
+        <TitleHeader />
+
+        <InputFields
+          userID={userID}
+          rivalIDs={rivalIDs}
+          period={period}
+          onInputChange={handleInputChange}
+        />
+
+        <Plot
+          className="w-full mt-10"
+          data={data}
+          layout={layout}
+          config={config}
+          onHover={(data) => {
+            console.log(data);
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default function Home() {
+  return (
+    <div className="m-2">
+      <Content />
+    </div>
   );
 }
