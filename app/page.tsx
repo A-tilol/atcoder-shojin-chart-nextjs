9272d2b2811130bc521b5c05fcaff8cf2f1de2cb
atilol <ggrkssk@gmail.com>
2025-01-01 18:55:34 +0900

    swap snuke chokudai

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ const InputFields: React.FC<InputFieldsProps> = ({
           id="user-id-input"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
           value={userID}
-          placeholder="chokudai"
+          placeholder="snuke"
           onChange={handleChange}
         />
       </div>
@@ -94,7 +94,7 @@ const InputFields: React.FC<InputFieldsProps> = ({
           id="rival-ids-input"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
           value={rivalIDs}
-          placeholder="snuke, rng"
+          placeholder="chokudai, rng"
           onChange={handleChange}
         />
       </div>
